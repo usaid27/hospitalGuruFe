1fb78ca6a9fde35c0d9d9af2c010ee1a2cc29282
Ankit Sharma <ankit.sharma@credentinfotech.com>
2024-10-18 12:40:33 +0530

    Ankit 8th data reflection doctor, hospital and procedure section and userProfile page

--- src/Pages/Admin/Users.js
+++ src/Pages/Admin/Users.js
@@ -11,9 +11,16 @@ function Users() {
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
+  const [userName, setuserName] = useState("");
+  const [Email, setEmail] = useState("");
+  const [MobileNumber, setMobileNumber] = useState("");
+  const [Rpassword, setRPassword] = useState("");
+  const [NewConfirmPassword, setNewConfirmPassword] = useState("");
+  //   const [confirmPassword, setConfirmPassword] = useState("");
+
   const handleTabChange = (tab) => setActiveTab(tab);
 
-  const handleSubmit = async (e) => {
+  const handlechangeSubmit = async (e) => {
     e.preventDefault();
 
     // Password Validation: Ensure newPassword and confirmPassword match
@@ -38,8 +45,58 @@ function Users() {
         `${baseUrl}/api/Account/ChangePassword`,
         submitData
       );
-      toast.success("Password changed successfully!");
       console.log(response);
+      if (response.status == 200) {
+        toast.success("Password changed successfully!");
+      }
+
+      // Clear form after successful submission
+      setcurrentPassword("");
+      setNewPassword("");
+      setConfirmPassword("");
+    } catch (error) {
+      toast.error("Failed to change password. Please try again.");
+      console.error(error);
+    }
+  };
+  const handleAddSubmit = async (e) => {
+    e.preventDefault();
+
+    // Password Validation: Ensure newPassword and confirmPassword match
+    if (
+      !userName ||
+      !Email ||
+      !MobileNumber ||
+      !Rpassword ||
+      !NewConfirmPassword
+    ) {
+      toast.error("Please fill in all fields.");
+      return;
+    }
+
+    if (Rpassword !== NewConfirmPassword) {
+      toast.error("password and confirm password do not match.");
+      return;
+    }
+
+    const submitData = {
+      userName: userName,
+      email: Email,
+      mobileNo: MobileNumber,
+      password: Rpassword,
+      passwordConfirm: NewConfirmPassword,
+      isActive: true,
+    };
+
+    try {
+      const response = await axios.post(
+        `${baseUrl}/api/Account/register`,
+        submitData
+      );
+      console.log(response);
+      if (response.status == 200) {
+        toast.success("User Register successfully!");
+      }
 
       // Clear form after successful submission
       setcurrentPassword("");
@@ -74,6 +131,15 @@ function Users() {
               Change Password
             </a>
           </li>
+          <li className="nav-item">
+            <a
+              className={`nav-link ${activeTab === "AddUser" ? "active" : ""}`}
+              href="#"
+              onClick={() => handleTabChange("AddUser")}
+            >
+              Add User
+            </a>
+          </li>
         </ul>
       </div>
 
@@ -102,10 +168,10 @@ function Users() {
             <p>Email: John.Doe@credentialinfotech.com</p>
             <p>Hospital: Hospital Guru</p>
           </div>
-        ) : (
+        ) : activeTab === "password" ? (
           <div className="change-password bg-white p-4 shadow-sm">
             <h5 className="text-center mt-2">Change Password</h5>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handlechangeSubmit}>
               <div className="mb-3">
                 <label className="form-label">Current Password</label>
                 <input
@@ -150,6 +216,85 @@ function Users() {
                     setcurrentPassword("");
                     setNewPassword("");
                     setConfirmPassword("");
+                  }}
+                >
+                  Clear
+                </button>
+              </div>
+            </form>
+          </div>
+        ) : (
+          <div className="change-password bg-white p-4 shadow-sm">
+            <h5 className="text-center mt-2">Add User</h5>
+            <form onSubmit={handleAddSubmit}>
+              <div className="mb-3">
+                <label className="form-label">User Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter your User Name"
+                  value={userName}
+                  onChange={(e) => setuserName(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Enter your Email"
+                  value={Email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Mobile No</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  placeholder="Enter your Mobile Number"
+                  value={MobileNumber}
+                  onChange={(e) => setMobileNumber(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Enter your password"
+                  value={Rpassword}
+                  onChange={(e) => setRPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Confirm Password</label>
+                <input
+                  type="Password"
+                  className="form-control"
+                  placeholder="Confirm Password"
+                  value={NewConfirmPassword}
+                  onChange={(e) => setNewConfirmPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="d-flex justify-content-end">
+                <button type="submit" className="btn btn-primary me-2">
+                  Register User
+                </button>
+                <button
+                  type="reset"
+                  className="btn btn-secondary"
+                  onClick={() => {
+                    setuserName("");
+                    setEmail("");
+                    setMobileNumber("");
+                    setRPassword("");
+                    setNewConfirmPassword("");
                   }}
                 >
                   Clear
